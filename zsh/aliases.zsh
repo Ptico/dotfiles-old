52f695dfe5c03294118909b3486fc855d3341f0c
Andrey Savchenko <andrey@aejis.eu>
2010-02-23 18:48:48 +0200

    More aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,10 +2,15 @@
 # Aliases
 #
 alias ..='cd ..'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
 
 alias mv='nocorrect mv -v'
 alias cp='nocorrect cp -v'
+alias cpf='nocorrect cp -R'
 alias rm='nocorrect rm -v'
+alias rmf='nocorrect rm -rfv'
 alias mc='mc --nocolor'
 alias mkdir='nocorrect mkdir'
 alias man='nocorrect man'
@@ -25,7 +30,7 @@ alias mkinst='sudo make install'
 alias mnt='sudo mount'
 alias mntl='sudo mount -o loop'
 alias umnt='sudo umount'
-alias h=history
+alias h='history'
 alias d='dirs -v'
 alias ssync="rsync --rsh=ssh"
 alias ssyncr="rsync --rsh=ssh --recursive --verbose --progress"
@@ -33,10 +38,27 @@ alias grab="sudo chown ${USER} --recursive"
 alias clean="rm *~"
 alias grep='grep --color=auto'
 
+# Rails 2
+alias rs='script/server'
+alias rc='script/console'
+alias rg='script/generate'
+alias rdb='script/dbconsole'
+
+# Rails 3
+alias r='rails'
+
+# Global
 alias -g M='|more'
 alias -g L='|less'
 alias -g H='|head'
 alias -g T='|tail'
 alias -g N='2>/dev/null'
 
+# Filetypes
+alias -s conf="vim"
+alias -s ini="vim"
+alias -s cnf="vim"
+alias -s cfg="vim"
 alias -s log="tail -50"
+alias -s gz="tar xzf"
+alias -s bz2="tar xjf"
